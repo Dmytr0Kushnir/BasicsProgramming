01237fe2c905c485e14e6d13cbdffefe2cac1880
Dmytr0Kushnir <78971232+Dmytr0Kushnir@users.noreply.github.com>
2021-03-04 02:08:32 +0200

    Самостійна

---
+++ Classwork/Project1/Project1/main.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include<stdio.h>
+#include<windows.h>
+
+using namespace std;
+//int main()
+//{
+//	SetConsoleOutputCP(1251);
+//	SetConsoleCP(1251);
+//	float a, b, c;
+//	cin >> a >> b >> c;
+//	float d = b * b - 4 * a * c;
+//	if (d > 0)
+//	{
+//		float x1 = ((-b + sqrt(d)) / 2 * a);
+//		float x2 = ((-b - sqrt(d)) / 2 * a);
+//		cout << "x1 = " << x1 << " " << "x2 = " << x2;
+//	}
+//	else if (d < 0)
+//	{
+//		cout << "Коренів не існує";
+//	}
+//	else if (d == 0)
+//	{
+//		float x = -b  / 2 * a;
+//		cout << "x = " << x;
+//	}
+//	return 0;
+//}
+/*int who_call;
+	cout << "who is calling you? " << "\n";
+	cin >> who_call;
+	if (who_call == 1) {
+		cout << "Bro let*s go playing football" << "\n";
+	}
+	else if (who_call == 2) {
+		cout << "Bro C++ lego, lego" << "\n";
+	}
+	else {
+		cout << "ZZZzzzzzzz" << "\n";
+	}*/
+int main() {
+	int a;
+	a = 48;
+	a = 87;
+	char d = 0;
+	cout << a << "\n";
+	cout << d << "\n";
+	bool zero = 1;
+	if (d) {
+		cout << 45;
+	
+	}
+	else {
+		cout << 12;
+	}
+}
